02f0110184f7eee2c14b0e0810fb178554455096
Moryah D <moryah.dick@ocsbstudent.ca>
2019-10-02 14:04:15 -0400

    Addded touch and react

--- TouchDragMoryah/main.lua
+++ TouchDragMoryah/main.lua
@@ -1,7 +1,17 @@
 -----------------------------------------------------------------------------------------
---
+-- Title: TouchAndDrag
+-- Name: Your Name
+-- Course: ICS2O/3C
+-- This program moves images when you press on them and doesn't stick to others
+-- dragged over them
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- loocal variables. i am sill trying to get the x-value to be set properly
+local backgroundImage = display.newImageRect("Images/space-2.png", 1660, 1500)
