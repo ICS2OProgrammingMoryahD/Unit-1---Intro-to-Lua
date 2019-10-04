46f7e109d04c9d067dde26cf898b8f2899a2f9e5
Moryah D <moryah.dick@ocsbstudent.ca>
2019-10-04 15:23:37 -0400

    finished touch drag

--- NumTextFeildMoryah/main.lua
+++ NumTextFeildMoryah/main.lua
@@ -65,7 +65,7 @@ local function NumericFieldListener( event )
 		-- if the user answer and the correct answer are the same:
 		if (userAnswer == correctAnswer) then
 			correctObject.isVisible = true
-			timerperformWithDelay(2000, HideCorrect)
+			timer.performWithDelay(2000, HideCorrect)
 
 		end
 	end
@@ -93,9 +93,32 @@ numericField.inputType = "number"
 
 -- add the event listen for the numeric field
 numericField:addEventListener( "userInput", NumericFieldListener)
+
 -----------------------------------------------------------------------------------------
 -- FUNTION CALLS
 -----------------------------------------------------------------------------------------
 
 --call the function to ask the question
 AskQuestion()
+
+-----------------------------------------------------------------------------------------
+-- LOCAL VARIBLES
+-----------------------------------------------------------------------------------------
+
+local points = 0
+
+-----------------------------------------------------------------------------------------
+-- OBJECT CREATION
+-----------------------------------------------------------------------------------------
+
+-- display the amount of points as a text object
+pointsText = display.newText("point = " .. points, display.contentWidth/3, display.
+	contentHeight/3, nil, 50)
+
+if (userAnswer == correctAnswer ) then
+	-- give a point if user gets the correct answer
+	points = points + 1
+
+	-- update it in the display object
+	pointsText.text = "points = " .. points
+end
--- TouchDragMoryah/main.lua
+++ TouchDragMoryah/main.lua
@@ -14,4 +14,48 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 -- loocal variables. i am sill trying to get the x-value to be set properly
-local backgroundImage = display.newImageRect("Images/space-2.png", 1660, 1500)
+local backgroundImage = display.newImageRect("Images/space-2.png", 2480, 1536)
+local ship = display.newImageRect("Images/spaceship.png", 350, 300)
+local shipWidth = ship.width
+local shipHeight = ship.shipHeight
+
+local astro = display.newImageRect("Images/astronaut.png", 250, 250)
+local astroWidth = astro.width
+local astroHeight = astro.shipHeight
+
+-- my boolean variables to keep track of which object i touched first
+local alreadyTouchedAstro = false
+local alreadyTouchedShip = false
+
+-- set the initial x and y  position of myImage
+astro.x = 300
+astro.y = 500
+
+ship.x = 700
+ship.y = 300
+
+-- function: astroListener
+-- Input: touch listener
+-- Output: none
+-- Description: when astronaut is touched, move initial
+local function astroListener(touch)
+
+	if (touch.phrase == "began") then
+		if (alreadyTouchedShip == false) then
+			alreadyTouchedAstro = true
+		end
+	end
+
+	if ( (touch.phase == "moved") and (alreadyTouchedAstro == true) ) then
+		astro.x = touch.x
+		astro.y = touch.y
+	end
+
+	if (touch.phase == "ended") then
+		alreadyTouchedAstro = false
+		alreadyTouchedShip = false
+	end
+end
+
+-- add the respective listeners to each object
+astro:addEventListener("touch", astroListener)
