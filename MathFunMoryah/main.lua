5c1e777ec022b782334ead4fcc4cca64dbf3a5a2
melody <melody.berhane@ocsbstudent.ca>
2019-10-08 00:15:40 -0400

    edited math fun

--- MathFunMoryah/main.lua
+++ MathFunMoryah/main.lua
@@ -1,9 +1,9 @@
 -----------------------------------------------------------------------------------------
---
--- Title: NumericTextFields
--- Name: MoryaH
+-- Title: MathFun
+-- Name: Your Name
 -- Course: ICS2O/3C
--- This program displays a math question and ask the user to answer in a numeric textfield.
+-- This program creates math problems with random operators
+-- (*,-,+,/)
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
@@ -12,40 +12,61 @@
 display.setStatusBar(display.HiddenStatusBar)
 
 -- sets the background colour
-display.setDefault("background", 124/255, 249/255, 199/255)
-
+display.setDefault("background", 1/255, 1/255, 55/255)
+ 
 -----------------------------------------------------------------------------------------
 -- LOCAL VARIABLES
 -----------------------------------------------------------------------------------------
 
--- create local variables
+local randomOp
+local num1
+local num2
 local questionObject
+local userAns
+local correctAns
 local correctObject
-local numericField
-local randomNumber1
-local randomNumber2
-local userAnswer
-local correctAnswer
 local incorrectObject
-local numIncorrect = 0
-local points = 0
-local win
-local lose
+local numericField
+
 
 -----------------------------------------------------------------------------------------
 -- LOCAL FUNTIONS
 -----------------------------------------------------------------------------------------
 
 local function AskQuestion()
-	-- generate 2 random number between a max. and a min. number
-	randomNumber1 = math.random(0, 10)
-	randomNumber2 = math.random(0,10)
 
-	correctAnswer = randomNumber1 + randomNumber2	
-
-	-- create question in text object
-	questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
-
+	-- chose a random operators
+	randomOp = math.random(1,4)
+	
+	if (randomOp == 1) then
+		-- generate 2 random number between a max. and a min. number
+		num1 = math.random(0, 10)
+		num2 = math.random(0, 10)
+		correctAnswer = num1 + num2
+		-- create question in text object
+		questionObject.text = num1 .. "+" .. num2 .. " = "
+	elseif (randomOp == 2) then
+		-- generate 2 random number between a max. and a min. number
+		num1 = math.random(0, 10)
+		num2 = math.random(0, 10)
+		correctAnswer = num1 - num2	
+		-- create question in text object
+		questionObject.text = num1 .. "-" .. num2 .. " = "
+	elseif (randomOp == 3) then
+		-- generate 2 random number between a max. and a min. number
+		num1 = math.random(0, 10)
+		num2 = math.random(0, 10)
+		correctAnswer = num1 * num2	
+		-- create question in text object
+		questionObject.text = num1 .. "*" .. num2 .. " = "
+	elseif (randomOp == 4) then
+		-- generate 2 random number between a max. and a min. number
+		num1 = math.random(10, 100)
+		num2 = math.random(1, 10)
+		correctAnswer = num1 / num2	
+		-- create question in text object
+		questionObject.text = num1 .. "/" .. num2 .. " = "
+	end
 end
 
 local function HideCorrect()
@@ -54,47 +75,31 @@ local function HideCorrect()
 	AskQuestion()
 end
 
-local function NumericFieldListener( event )
-
-	-- User begins editing "numericField"
+local function NumericFieldListener( event ) -- User begins typing in "numericField"
+	
 	if ( event.phase == "began" ) then
-		
-		-- when the answer is submitted (enter key is pressed) set user input to user's
+	
+	-- when the answer is submitted (enter key is pressed) set user input to user's
 	elseif (event.phase == "submitted") then
 
-		-- answer
+		-- changes the user's answer into numbers
 		userAnswer = tonumber(event.target.text)
 
 		-- if the user answer and the correct answer are the same:
 		if (userAnswer == correctAnswer) then
+			
 			-- show the correct object
 			correctObject.isVisible = true
-
-			-- give a point if user gets the correct answer
-			points = points + 1
-			-- update it in the display object
-			pointsText.text = "points = " .. points
+			
 			-- add timer
 			timer.performWithDelay(2000, HideCorrect)
-			-- show "you win!" after 5 points		
-			if (points == 5) then
-				correctObject.isVisible = false
-				winObject.isVisible = true
-				timer.performWithDelay(2000, HideWin)
-			end
-		-- user answer is incorrect
-		else
+		
+		else -- user answer is incorrect
+			
 			-- display incorrectObject when user gets answer wrong
 			incorrectObject.isVisible = true
 			timer.performWithDelay(2000, HideCorrect)
-			-- everytime user gets answer wrong add 1 to numIncorrect
-			numIncorrect = numIncorrect + 1
-			-- display game over after 3 wrong answers
-			if (numIncorrect == 3) then
-				incorrectObject.isVisible = false
-				loseObject.isVisible = true
-				timer.performWithDelay(2000, HideLose)
-			end
+			
 		end
 		-- clear text field
 		event.target.text = ""
@@ -107,34 +112,22 @@ end
 
 -- displays a question and sets the colour
 questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/2,
- nil, 50 )
-questionObject:setTextColor(155/255, 44/255, 198/255)
+ nil, 60 )
+questionObject:setTextColor(55/255, 55/255, 255/255)
 
--- create the correct text object object and make it invisible
+-- create the correct text object and make it invisible
 correctObject = display.newText( "Correct!", display.contentWidth/2, 
 	display.contentHeight
-*2/3, nil, 50 )
-correctObject:setTextColor(155/255, 42/255, 198/255)
+*2/3, nil, 77 )
+correctObject:setTextColor(55/255, 255/255, 55/255)
 correctObject.isVisible = false
 
 -- create the incorrect text object object and make it invisible
 incorrectObject = display.newText( "Incorrect!", display.contentWidth/2, 
 	display.contentHeight
-*2/3, nil, 50 )
-incorrectObject:setTextColor(155/255, 42/255, 198/255)
+*2/3, nil, 77 )
+incorrectObject:setTextColor(255/255, 55/255, 55/255)
 incorrectObject.isVisible = false
-
--- create the win text object object and make it invisible
-winObject = display.newImageRect("Images/win.png", 450, 300)
-winObject.x =  512
-winObject.y = 590
-winObject.isVisible = false
-
--- create the lose text object object and make it invisible
-loseObject = display.newImageRect("Images/lose.png", 450, 290)
-loseObject.x =  512
-loseObject.y = 590
-loseObject.isVisible = false
 
 -- Create numeric field
 numericField = native.newTextField( display.contentWidth/2, display.contentHeight/2, 150,
@@ -143,10 +136,6 @@ numericField.inputType = "number"
 
 -- add the event listen for the numeric field
 numericField:addEventListener( "userInput", NumericFieldListener)
-
--- display the amount of points as a text object
-pointsText = display.newText("point = " .. points, display.contentWidth/3, display.
-	contentHeight/3, nil, 50)
 
 -----------------------------------------------------------------------------------------
 -- FUNTION CALLS
